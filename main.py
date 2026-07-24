313e395e4a3895c69c82a74fe75ac7929a7b93eb
Sharana Basava M <sharanabasavabasava841@gmail.com>
2026-07-25 00:00:48 +0530

    Implement weather notification via Twilio
    Replaced email birthday notification with weather alert using Twilio API.

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+APIKEY = os.environ.get("APIKEY")
+phone_num = os.environ.get('phone_num')
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+account_sid = os.environ.get('account_sid')
+auth_token = os.environ.get('auth_token')
+parm={
+     "lat":15.139393,
+    "lon": 76.921440,
+    "appid":APIKEY,
+    "cnt":4
+}
+respone = requests.get(OWM_endpoint,params=parm)
+respone.raise_for_status()
+weather_data = respone.json()
+weather_data_list=[]
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+will_rain = False
+for i in range(len(weather_data["list"])):
+    condition_code = weather_data["list"][i]["weather"][0]["id"]
+    if condition_code < 700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body = "It's going to rain today. Remember to take an umbrella☂️",
+        to = phone_num
+    )
